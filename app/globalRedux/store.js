ebc7f08fd3f5c91da13c6850eccd14fa6ef7ed86
B-mitchell <mitchellonuorah77@gmail.com>
2024-09-22 21:29:58 +0100

    fixed errors

--- app/globalRedux/store.js
+++ app/globalRedux/store.js
@@ -1,21 +1,17 @@
-'use client'
-import { configureStore } from '@reduxjs/toolkit'
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-import rootReducer from './rootReducer';
+import { configureStore } from '@reduxjs/toolkit';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // Import storage from redux-persist
+import rootReducer from './rootReducer'; // Assuming you have a root reducer
 
 const persistConfig = {
-    key: 'root',
-    storage,
+  key: 'root',
+  storage,
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-    reducer: persistedReducer
-})
+  reducer: persistedReducer,
+});
 
-// export const RootState = store.getState;
 export const persistor = persistStore(store);
-export const AppDispatch = store.dispatch;
-export default store;
